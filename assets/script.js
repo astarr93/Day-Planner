59fc9eea7d8ce96abd3be296fac711292a290049
Andrew Starr <astarr93@outlook.com>
2020-06-22 04:37:53 -0400

    added global variable starting array for dynamic div building and stored a get for the html container

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
-// Javascript
+// Dynamically create a div for each working hour of an 8-5 shift:
+const workingHours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+const container = $("container");
+
+$(document).ready(function () {
+  //   const $currentDay = $("#currentDay");
+  //   const $container = $(".container");
+  //   let nowDay = moment().format("dddd, MMMM Do");
+  //   $currentDay.text(nowDay);
+  //   $(".saveBtn").on("click", function () {
+  //     var activity = $(this).siblings(".description").val();
+  //     var hour = $(this).parent().attr("id");
+  //     localStorage.setItem(hour, activity);
+  //     console.log(localStorage);
+  //   });
+});
