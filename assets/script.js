4bc7bdc25dd8e44651aff534f07179911a9f1118
Andrew Starr <astarr93@outlook.com>
2020-06-23 01:52:59 -0400

    Added JS logic to dynamically build time blocks on HTML with text area and save button

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,40 @@
-// Dynamically create a div for each working hour of an 8-5 shift:
-const workingHours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+//Global Variables
+const container = $(".container");
+const workingHours = [
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+];
 
-const container = $("container");
-
+//When the DOM is ready, JS builds each time-block using the woork
 $(document).ready(function () {
-  //   const $currentDay = $("#currentDay");
-  //   const $container = $(".container");
-  //   let nowDay = moment().format("dddd, MMMM Do");
-  //   $currentDay.text(nowDay);
-  //   $(".saveBtn").on("click", function () {
-  //     var activity = $(this).siblings(".description").val();
-  //     var hour = $(this).parent().attr("id");
-  //     localStorage.setItem(hour, activity);
-  //     console.log(localStorage);
-  //   });
+  $.each(workingHours, function (index, value) {
+    container.append($('<div class="row time-block">'));
+    console.log(index);
+  });
+  $(".time-block").append($('<div class="col-md-1 col-sm-2 hour">'));
+  $(".time-block").append(
+    $('<textarea class="description col-md-10 col-sm-8">')
+  );
+  $(".time-block").append($('<button class="saveBtn col-md-1 col-sm-2">'));
+  $(".saveBtn").append($('<i class="far fa-save">'));
+  // addHours();
 });
+
+//   const $currentDay = $("#currentDay");
+//   const $container = $(".container");
+//   let nowDay = moment().format("dddd, MMMM Do");
+//   $currentDay.text(nowDay);
+//   $(".saveBtn").on("click", function () {
+//     var activity = $(this).siblings(".description").val();
+//     var hour = $(this).parent().attr("id");
+//     localStorage.setItem(hour, activity);
+//     console.log(localStorage);
+//   });
