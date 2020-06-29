31efb6bb8daf7e87e9d2b3b20857e1e170112de8
Andrew Starr <astarr93@outlook.com>
2020-06-29 11:54:22 -0400

    refactored js logic

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 //Global Variables
 const container = $(".container");
-const workingHours = [
+let currentHour = moment().hour();
+let bookedEvents = [];
+const workingHours = [  // Add additional hours to dynamically create all time-block components
   "8am",
   "9am",
   "10am",
@@ -13,54 +15,74 @@ const workingHours = [
   "5pm",
 ];
 
-//When the DOM is ready, JS builds each time-block using the woork
+// WHEN DOM IS LOADED, THESE FUNCTIONS ARE CALLED TO BUILD THE DAY SCHEDULER
 $(document).ready(function () {
-  $.each(workingHours, function () {
-    container.append($('<div class="row time-block">'));
-  });
-  $(".time-block").append($('<div class="col-md-1 col-sm-2 hour">'));
-  $(".time-block").append(
-    $('<textarea class="description col-md-10 col-sm-8">')
-  );
-  $(".time-block").append($('<button class="saveBtn col-md-1 col-sm-2">'));
-  $(".saveBtn").append($('<i class="far fa-save">'));
-  displayDate();
-  addHours();
-  loadActivity();
+  displayToday();
+  buildScheduler();
+  loadScheduledEvents();
+  addSaveClickEvent();
+  // compareHour();
 });
 
+// FUNCTIONS:
+
 // Displays current date on index header
-function displayDate() {
-  let Today = moment().format("dddd, MMMM Do");
+function displayToday() {
+  let Today = moment().format("LLLL");
   $("#currentDay").text(Today);
 }
 
-// Add unique class hour-a and write in the hour for each timeblock hour div;
-function addHours() {
-  $(".hour").each(function (i) {
-    $(this).attr("id", "hour-" + i);
+// Builds a time-slot for each index of workingHours
+function buildScheduler() {
+  $.each(workingHours, function () {
+    container.append($('<div class="row time-block">'));
   });
-  let a = 0;
-  for (i = 0; i < workingHours.length; i++) {
-    $(".hour-" + a).text(workingHours[a]);
-    a += 1;
-  }
+  // Appends columns, textareas, and save buttons to each row created from workingHours
+  $(".time-block").append($('<div class="col-md-1 col-sm-2 hour">'));
+  $(".time-block").append($('<textarea class="description col-md-10 col-sm-8">'));
+  $(".time-block").append($('<button class="saveBtn col-md-1 col-sm-2">'));
+  $(".saveBtn").append($('<i class="far fa-save">'));
+  addTimesAndIds();
 }
 
-// Add unique class activity-a for each text area. A's value should match the same value of the hour class in the sibling div from addHours()
-function loadActivity() {
-  let a = 0;
-  for (i = 0; i < workingHours.length; i++) {
-    $("#hour-" + a)
-      .siblings("textarea")
-      .attr("id", "activity-" + a);
-    a++;
-  }
+// Writes in defined workingHours into Scheduler and adds unique Ids
+function addTimesAndIds() {
+  $(".hour").each(function (i) {
+    $(this).attr("id", workingHours[i]).html(workingHours[i]);
+    $(this).siblings("textarea").attr("id", workingHours[i]);
+  });
 }
 
-$(".saveBtn").on("click", function () {
-  // let activity = $(this).siblings(".description").val();
-  // let hour = $(this).siblings().attr("class");
-  // localStorage.setItem(hour, activity);
-  console.log("success");
-});
+// Load any previously scheduled events
+function loadScheduledEvents() {
+  let bookedEvents = {}
+  keys = Object.keys(localStorage)
+  i = keys.length;
+
+}
+
+// Add click event handler for the hour block's save handler. Save's textarea contents to local storage
+function addSaveClickEvent() {
+  $(".saveBtn").on("click", function () {
+    let eventTime = $(this).siblings(".hour").html();
+    let eventText = $(this).siblings(".description").val();
+    let appointment = [eventTime, eventText];
+    bookedEvents += appointment;
+  });
+}
+
+// Compare the current time against all timeslots and apply/remove specific classes based on past, present, and future
+// function compareHour() {
+//   let blockHour = parseInt($(this).attr("id").split("-")[1]);
+//   console.log(blockHour);
+//   console.log(currentHour);
+//   if (currentHour > blockHour) {
+//     $(this).siblings(".description").addClass("past");
+//   } else if (currentHour === blockHour) {
+//     $(this).siblings.removeClass("past");
+//     $(this).siblings.addClass("present");
+//   } else {
+//     $(this).siblings.removeClass("past present");
+//     $(this).siblings.addClass("future");
+//   }
+// }
